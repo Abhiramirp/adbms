7e6ef9ced152857a3ad9c57f339fa48eceb13387
Abhiramirp <148995237+Abhiramirp@users.noreply.github.com>
2024-04-22 15:09:23 +0530

    Update triggerrpgm.sql

--- triggerrpgm.sql
+++ triggerrpgm.sql
@@ -39,6 +39,20 @@ Table created.
 
 
 SQL> set serveroutput on
+
+  create or replace trigger t1 before insert or delete or update on customer for each row
+when(new.id>0)
+declare
+ sal_diff number;
+ begin
+ sal_diff:=:new.salary-:old.salary;
+ dbms_output.put_line('old salary'||:old.salary);
+ dbms_output.put_line('new salary'||:new.salary);
+ dbms_output.put_line('salary diff'||sal_diff);
+end;
+/
+
+  
 SQL> @triggerr.sql
 
 Trigger created.
